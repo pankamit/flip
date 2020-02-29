2121bad7cd43946e852e2ea0cf50b72c665e66a1
Pankaj Kumar Mittal <pankajmittalsoftware@gmail.com>
2020-02-29 13:19:49 +0530

    Adde Basic Structure for Jasper Report Generation

---
+++ JasperReportGenerateProject/src/main/java/com/flip/JasperReportGenerateProjectApplication.java
@@ -0,0 +1,13 @@
+package com.flip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JasperReportGenerateProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JasperReportGenerateProjectApplication.class, args);
+	}
+
+}
